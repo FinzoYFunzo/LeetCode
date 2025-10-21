4de56f5dd9501145a4a5c8bf16fc70b050e10d8e
FinzoYFunzo <joaquinandresparada@gmail.com>
2025-10-21 08:41:00 -0300

    1 - 1ms - 88.10%

--- 1_two_sum.ts
+++ 1_two_sum.ts
@@ -6,7 +6,7 @@ function twoSum(nums: number[], target: number): number[] {
   map[nums[0]] = 0
   const rest0 = target - nums[0];
 
-  for (let i = 1; i < nums.length; i++) { // compara 0 con 1, ..., n
+  for (let i = 1; i < nums.length; i++) { // compara 0 con 1, ..., n 
     if (nums[i] == rest0) return [0, i]
     map[nums[i]] = i                      // llena hash table
   }
