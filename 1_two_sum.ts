011431cf5d0bb69cddfebed9533f53d023577a6b
FinzoYFunzo <joaquinandresparada@gmail.com>
2025-10-21 08:40:20 -0300

    1 - 1ms - Beats - 88.10%

--- 1_two_sum.ts
+++ 1_two_sum.ts
@@ -6,14 +6,14 @@ function twoSum(nums: number[], target: number): number[] {
   map[nums[0]] = 0
   const rest0 = target - nums[0];
 
-  for (let i = 1; i < nums.length; i++) {
+  for (let i = 1; i < nums.length; i++) { // compara 0 con 1, ..., n
     if (nums[i] == rest0) return [0, i]
-    map[nums[i]] = i
+    map[nums[i]] = i                      // llena hash table
   }
 
   for (let j = 1; j < nums.length; j++) {
-    const rest = target - nums[j]
-    const restIndex = map[rest]
+    const rest = target - nums[j]         
+    const restIndex = map[rest]           // busca el resto de j en la hash table
     if (restIndex && restIndex != j) return [j, map[rest]]
   }
 
